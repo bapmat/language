fd4c348163ea3e1e2820616617747845f251c7bc
MM <66105482+bapmat@users.noreply.github.com>
2022-11-15 22:20:10 +0900

    변수

---
+++ python/basic/2.variable.py
@@ -0,0 +1,10 @@
+# 애완동물을 소개해 주세요
+animal = "고양이"
+name = "해피"
+age = 4     # 정수형 문자랑 출력할 때 str로 감싸기
+bobby = "낮잠"
+is_adult = age >= 3
+
+print("우리집 " + animal +"의 이름은 " + name +"에요")
+print(name +"는 " + str(age) + "살이며, " + bobby + "을 아주 좋아해요")
+print(name +"는 어른일까요? " + str(is_adult))
