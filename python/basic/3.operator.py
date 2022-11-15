7a79a326225995c71ec92897172b558c1443c00e
MM <66105482+bapmat@users.noreply.github.com>
2022-11-15 22:20:41 +0900

    연산자

---
+++ python/basic/3.operator.py
@@ -0,0 +1,50 @@
+print(1+1)
+print(3-2)
+print(5*2)
+print(6/3)
+
+print(2**3) # 2^3
+print(5%3)  # 나머지 구하기
+print(10%3)
+print(5//3) # 몫 구하기
+print(10//3)
+
+print(10 > 3)
+print(4 >= 7)
+print(10 < 3)
+print(5 <= 5)
+
+print(3 == 3) # 같다
+print(4 == 2)
+print(3 + 4 == 7)
+
+print(1 != 3) # 같지 않다
+print(not (1 != 3))
+
+print((3 > 0) and (3 < 5))
+print((3 > 0) & (3 < 5))
+
+print((3 > 0) or (3 > 5))
+print((3 > 0) | (3 > 5))
+
+print(5 > 4 > 3)
+print(5 > 4 > 7)
+
+# 수식
+print(2 + 3 * 4)
+print((2 + 3) * 4)
+
+number = 2 + 3 * 4
+print(number)
+number = number + 2
+print(number)
+number += 2
+print(number)
+number *= 2
+print(number)
+number /= 2
+print(number)
+number -= 2
+print(number)
+number %= 5 # 나머지 구하기
+print(number)
